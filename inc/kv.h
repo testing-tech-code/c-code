f180b7f6d2048903d7604df4d4f58b82d30b8025
testing-tech-code <jemailcollector@gmail.com>
2026-04-26 13:45:23 -0400

    revert

--- inc/kv.h
+++ inc/kv.h
@@ -16,7 +16,7 @@ typedef struct {
 } kv_t;
 
 kv_t *kv_init(size_t capacity);
-int kv_put(kv_t *db, const char *key, const char *value);
+int kv_put(kv_t *db, char *key, char *value);
 char  *kv_get(kv_t *db, char *key);
 int    kv_delete(kv_t *db, char *key);
 void   kv_free(kv_t *db);
